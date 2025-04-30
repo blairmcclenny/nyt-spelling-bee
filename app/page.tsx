491f28ebe248c0e979548f57ed1804bb3e0fba4e
Blair M McClenny <blairmmcclenny@Mac.lan>
2025-04-30 16:44:07 -0700

    create hive cells

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,25 @@
+"use client"
+
 export default function Home() {
-  return <div></div>
+  return (
+    <div className="relative">
+      <svg
+        viewBox="0 0 120 103.92304845413263"
+        className="max-w-64 absolute inset-0 fill-yellow-300"
+      >
+        <polygon
+          points="0,51.96152422706631 30,0 90,0 120,51.96152422706631 90,103.92304845413263 30,103.92304845413263"
+          onClick={() => console.log("clicked")}
+        />
+        <text
+          x="50%"
+          y="50%"
+          dy="0.35em"
+          className="fill-black [font-size:1.875em] font-bold [text-anchor:middle] uppercase pointer-events-none"
+        >
+          a
+        </text>
+      </svg>
+    </div>
+  )
 }
