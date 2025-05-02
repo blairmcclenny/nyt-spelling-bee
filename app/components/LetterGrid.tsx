ce347242ae6559dd6ccef1fec8c6954af6b628c5
Blair M McClenny <blairmmcclenny@Mac.lan>
2025-05-02 12:25:28 -0700

    letter grid with temp click event and colors

---
+++ app/components/LetterGrid.tsx
@@ -0,0 +1,78 @@
+"use client"
+
+type Position =
+  | "center"
+  | "topLeft"
+  | "top"
+  | "topRight"
+  | "bottomRight"
+  | "bottom"
+  | "bottomLeft"
+
+function Cell({
+  letter,
+  position,
+  onClick,
+}: {
+  letter: string
+  position: Position
+  onClick: () => void
+}) {
+  const positions = {
+    center: null,
+    topLeft: "-translate-x-3/4 -translate-y-1/2",
+    top: "-translate-y-full",
+    topRight: "translate-x-3/4 -translate-y-1/2",
+    bottomRight: "translate-x-3/4 translate-y-1/2",
+    bottom: "translate-y-full",
+    bottomLeft: "-translate-x-3/4 translate-y-1/2",
+  }
+
+  return (
+    <svg
+      viewBox="0 0 120 103.92304845413263"
+      className={`absolute top-1/3 left-[30%] w-[40%] h-1/3 fill-yellow-300 ${positions[position]}`}
+    >
+      <polygon
+        points="0,51.96152422706631 30,0 90,0 120,51.96152422706631 90,103.92304845413263 30,103.92304845413263"
+        className="stroke-white stroke-[7.5] cursor-pointer"
+        onClick={onClick}
+        role="button"
+        aria-label={`${letter} button`}
+      />
+      <text
+        x="50%"
+        y="50%"
+        dy="0.35em"
+        className="fill-black text-3xl font-bold [text-anchor:middle] uppercase pointer-events-none"
+      >
+        {letter}
+      </text>
+    </svg>
+  )
+}
+
+export default function LetterGrid({ letters }: { letters: string[] }) {
+  const positions: Position[] = [
+    "center",
+    "topLeft",
+    "top",
+    "topRight",
+    "bottomRight",
+    "bottom",
+    "bottomLeft",
+  ]
+
+  return (
+    <div className="w-full pb-[103.92304845413263%] relative">
+      {letters.map((letter, i) => (
+        <Cell
+          key={letter}
+          letter={letter}
+          position={positions?.[i]}
+          onClick={() => console.log(`${letter} clicked`)}
+        />
+      ))}
+    </div>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,11 @@
-"use client"
+import LetterGrid from "./components/LetterGrid"
 
 export default function Home() {
+  const letters = ["l", "t", "d", "u", "c", "i", "e"]
+
   return (
-    <div className="relative">
-      <svg
-        viewBox="0 0 120 103.92304845413263"
-        className="max-w-64 absolute inset-0 fill-yellow-300"
-      >
-        <polygon
-          points="0,51.96152422706631 30,0 90,0 120,51.96152422706631 90,103.92304845413263 30,103.92304845413263"
-          onClick={() => console.log("clicked")}
-        />
-        <text
-          x="50%"
-          y="50%"
-          dy="0.35em"
-          className="fill-black [font-size:1.875em] font-bold [text-anchor:middle] uppercase pointer-events-none"
-        >
-          a
-        </text>
-      </svg>
+    <div className="mx-auto max-w-64">
+      <LetterGrid letters={letters} />
     </div>
   )
 }
